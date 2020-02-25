8929c2278c409d2d12e3cccd8e68f7bd1ba47365
starkilla2003 <Starkilla@Starkilla-PC.localdomain>
2020-02-25 17:52:01 +0100

    Intial commit

---
+++ variables.tf
@@ -0,0 +1,24 @@
+variable "prefix" {
+  description = "The Prefix used for all resources in this example"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default     = "North Europe"
+}
+
+variable "appId" {
+  description = "appID of the service principal"
+}
+
+variable "password" {
+  description = "password of the service principal"
+}
+
+variable "tenant" {
+  description = "tenant id of the service principal"
+}
+
+variable "subscription_id" {
+  description = "subscription_id of the service principal"
+} 
